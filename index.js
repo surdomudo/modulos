c56c8f5ce466c604ddcb5311e0e6dffb056a11cb
surdomudo <140193686+surdomudo@users.noreply.github.com>
2024-07-05 19:53:08 +0000

    atividade sobre modulos

---
+++ index.js
@@ -0,0 +1,5 @@
+// const matematica = require("./matematica.mjs") CammomJS modules
+import {matematica} from "./matematica" //EsModule
+
+
+console.log(matematica.soma(2,2))
---
+++ matematica.js
@@ -0,0 +1,11 @@
+// EsModule export
+ export const matematica = {
+    soma: function(a,b){
+        return a+b
+    },
+    multiplicar: function(a,b){
+        return a*b
+    }
+}
+// module.exports = matematica; CommonJS modules
+
